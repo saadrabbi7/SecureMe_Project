3fd5594b4d8fe519fba6b35a9cd263a199c7533a
saadrabbi1050 <saadrabbi9@gmail.com.com>
2023-07-18 12:10:34 +0600

    Revert "secure me project"
    This reverts commit f1a489626d61209ed6ee34a42d21e7b92974f395.

--- resources/views/backend/layouts/partials/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<footer class="footer">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-6">
-               Developed by <a href="{{ url('/') }}">Secure Me</a>
-            </div>
-        </div>
-    </div>
-</footer>
