a6291c236b919332b8b83a34f8efd225b4a160cf
Fazla Rabbi Bhuiyan <58617186+saadrabbi1050@users.noreply.github.com>
2023-11-15 00:32:45 +0600

    EmergencyController.php

--- app/Http/Controllers/EmergencyController.php
+++ app/Http/Controllers/EmergencyController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Exception;
-use App\Models\Profile;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Location as ModelsLocation;
@@ -13,15 +12,48 @@ class EmergencyController extends Controller
 {
     public function sendAlert(Request $request)
     {
-        $phone = $request->relative_phone ?? null;
+        try {
+            // $ip = $request->ip();
+            // $ip = '103.121.10.163'; /* Static IP address */
+            // $getLocation = Location::get($ip);
+            // dd($getLocation);
+            // $loca = ModelsLocation::where('upazila', $getLocation->cityName)->first();
+            $url = $request->url;
 
-        $message = 'I am in Danger. My Location here :   https://' . $request->url . '';
+            
+            $msg = 'I am in Danger. My Location here :   https://'.$request->url.'';
+            
+            
+            $url = "http://portal.greenheritageit.com/smsapimany";
+            $data = [
+                "api_key" => "C200118761acdd771860b0.39556660",
+                "senderid" => "8809612441553",
+                "messages" => json_encode( [
+                    [
+                        "to" => $request->relative_phone ?? null,
+                        "message" => $msg
+                    ],
+                    [
+                        // "to" => "8801748173322",
+                        "to" => isset($request->police_station) ? $request->police_station : "",
+                        "message" => $msg
+                    ]
+                ])
+            ];
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            $response = curl_exec($ch);
+            curl_close($ch);
 
-        if (isset($phone)) {
-            Profile::sendSMS($phone, $message);
             return redirect()->back()->withSuccess('Send Successfully');
-        } else {
-            return redirect()->back()->withSuccess('Phone Number Not Found');
+
+
+        } catch (Exception $th) {
+            return redirect()->back()->withErrors($th->getMessage());
         }
     }
 }
